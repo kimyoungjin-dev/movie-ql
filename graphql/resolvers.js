e073fc561e777ea26197f76e8008b16610bfa1d1
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-27 15:38:28 +0900

    Receive information from graphql/db.js and write resolve code. Create Query and Mutation

---
+++ graphql/resolvers.js
@@ -0,0 +1,15 @@
+import { getMovies, getById, addMovie, deleteMovies } from "./db";
+
+const resolvers = {
+  Query: {
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+  },
+
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovies: (_, { id }) => deleteMovies(id),
+  },
+};
+
+export default resolvers;
